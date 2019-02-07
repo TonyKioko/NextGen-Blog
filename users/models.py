7c848c2c3b6378f5d849e401fa9e7d53d83bfc8e
TonyKioko <tonykioko384@gmail.com>
2019-02-07 14:43:22 +0300

    user profile image error solved

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpeg', upload_to='profile_pics/')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
