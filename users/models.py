06c2ccc12e350c1f19bbc348ec5dcfdb602b7734
TonyKioko <tonykioko384@gmail.com>
2019-02-07 20:32:45 +0300

    image resize method added

--- users/models.py
+++ users/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
