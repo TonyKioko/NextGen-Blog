99e81c5b77ce7ff0c5e6ecc803de35ce31eb0a9e
TonyKioko <tonykioko384@gmail.com>
2019-02-08 14:13:40 +0300

    can create posts

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.views.generic import (
     ListView,
     DetailView,
@@ -26,6 +28,18 @@ class PostDetailView(DetailView):
 
     model = Post
     template_name = 'blog/post_detail.html'  # <app>/<model>_<viewtype>.html
+
+# class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'  # <app>/<model>_<viewtype>.html
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
